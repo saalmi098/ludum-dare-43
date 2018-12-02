6366be7228011c586de0617664ff49df7eef529f
LompaT <andisalminger@hotmail.com>
2018-12-02 20:47:27 +0100

    Implemented Moving Platform

---
+++ Ludum Dare 43/Assets/MovingPlatformController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformController : MonoBehaviour {
+
+    public float moveDistance = 3f;
+
+	void FixedUpdate ()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time, moveDistance), transform.position.y, transform.position.z);
+	}
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground"))
+        {
+            collision.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground"))
+        {
+            collision.transform.parent = null;
+        }
+    }
+}
